8ef6bf281ab4631b3a2cc1b7c930963c8d963c0b
gprat <guillaumeprat@gmail.com>
2019-04-17 14:51:41 +0200

    service checkUser

--- sportGPJGNJ/src/app/services.ts
+++ sportGPJGNJ/src/app/services.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
 
-  constructor() { }
+  constructor(private http : Http) { }
 
   getLocalUser(){
     return new Date();
   }
   setLocalUser(){
     return new Date();
+  }
+
+  checkUser(user){
+    this.http.post('http://localhost:8080/adress', user).subscribe(data => {
+      console.log(data);
+    }, err => {
+      console.log(err);
+    });
   }
 }
