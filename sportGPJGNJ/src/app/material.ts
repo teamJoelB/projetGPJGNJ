fe681a7dfdb911fa2d62ee2fe8b46dbd56e4f967
JulienB <gh0stika@hotmail.fr>
2019-04-17 14:33:00 +0200

    material update

--- sportGPJGNJ/src/app/material.ts
+++ sportGPJGNJ/src/app/material.ts
@@ -3,7 +3,7 @@ import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatSelectModule} from '@angular/material/select';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
     imports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatSelectModule, MatCardModule],
