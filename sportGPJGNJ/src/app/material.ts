2a44ca51a72a382975fcd33f88aab16ddfcaa842
JulienB <gh0stika@hotmail.fr>
2019-04-17 14:20:57 +0200

    update material

--- sportGPJGNJ/src/app/material.ts
+++ sportGPJGNJ/src/app/material.ts
@@ -1,12 +1,13 @@
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
-    imports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatSelectModule],
-    exports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatSelectModule],
+    imports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatSelectModule, MatCardModule],
+    exports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatSelectModule, MatCardModule],
 })
 
 export class MaterialModule { }
